76d336794d42ce0344994c070d713fd325357343
longnguyen5 <nguyntnlong12@gmail.com>
2023-11-27 09:24:18 +0700

    Add project files.

---
+++ DoAnSimple/DataServices.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// mở các thư viện
+using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DrugStoreManagement
+{
+    class DataServices
+    {
+        private static SqlConnection mySqlConnection;
+        private SqlDataAdapter mySqlDataAdapter;
+
+        // Connect to the database
+        public bool OpenDB()
+        {
+            string conStr = "Data Source=LAPTOP-VHVPC4RM\\SQLEXPRESS;Initial Catalog=Mart;Integrated Security=True";
+            try
+            {
+                mySqlConnection = new SqlConnection(conStr);
+                mySqlConnection.Open();
+            }
+            catch (SqlException ex)
+            {
+                DisplayError(ex);
+                mySqlConnection = null;
+                return false;
+            }
+            return true;
+        }
+
+        // Query data into a DataTable
+        public DataTable RunQuery(string query, params SqlParameter[] parameters)
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                SqlCommand cmd = new SqlCommand(query, mySqlConnection);
+                cmd.Parameters.AddRange(parameters);
+                mySqlDataAdapter = new SqlDataAdapter(cmd);
+                SqlCommandBuilder mySqlCommandBuilder = new SqlCommandBuilder(mySqlDataAdapter);
+                mySqlDataAdapter.Fill(dt);
+            }
+            catch (SqlException ex)
+            {
+                DisplayError(ex);
+                return null;
+            }
+            return dt;
+        }
+
+        // Update a DataTable to a database table
+        public void Update(DataTable dt)
+        {
+            try
+            {
+                mySqlDataAdapter.Update(dt);
+            }
+            catch (SqlException ex)
+            {
+                DisplayError(ex);
+            }
+        }
+
+        // Execute a SQL statement such as insert, update and delete
+        public void ExecuteNonQuery(string sSql, params SqlParameter[] parameters)
+        {
+            SqlCommand mySqlCommand = new SqlCommand(sSql, mySqlConnection);
+            mySqlCommand.Parameters.AddRange(parameters);
+            try
+            {
+                mySqlCommand.ExecuteNonQuery();
+            }
+            catch (SqlException ex)
+            {
+                DisplayError(ex);
+            }
+        }
+
+        public void DisplayError(SqlException ex)
+        {
+            string sSql = "SELECT * FROM Errors WHERE Number = @Number";
+            DataTable dtError = RunQuery(sSql, new SqlParameter("@Number", ex.Number));
+            if (dtError.Rows.Count > 0)
+                MessageBox.Show(dtError.Rows[0][1].ToString().Trim(), "Error " + ex.Number.ToString(), MessageBoxButtons.OK, MessageBoxIcon.Information);
+            else
+                MessageBox.Show(ex.Message, "Error " + ex.Number.ToString(), MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        internal int ExecuteScalar(string ordersSql, object ordersParameters)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
---
+++ DoAnSimple/Login.cs
@@ -0,0 +1,68 @@
+﻿using DrugStoreManagement;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DoAnSimple
+{
+    public partial class Login : Form
+    {
+        // khai báo một biến lớp 
+        private DataServices myDataServices;
+        private SqlCommand mySqlCommand;
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            myDataServices = new DataServices();
+            // kết nối đến CSDL
+            if (myDataServices.OpenDB() == false)
+            {
+                return;
+            }
+            int acctype, id;
+            // truy vấn bảng User
+            string sSql = "Select * from [User] Where Username = @uname AND Password = @pword";
+            DataTable dt = myDataServices.RunQuery(sSql, new SqlParameter("@uname", txtUsername.Text), new SqlParameter("@pword", txtPassword.Text));
+            // kiem tra username va password
+            if (dt.Rows.Count == 0)
+            {
+                MessageBox.Show("Không đúng tài khoản/mật khẩu đăng nhập!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtUsername.Focus();
+                return;
+            }
+            else
+            {
+                id = Convert.ToInt32(dt.Rows[0]["UserID"]);
+                acctype = Convert.ToInt32(dt.Rows[0]["AccountType"]);
+            }
+            if (acctype == 0) // Nhân viên
+            {
+                Main frm = new Main();
+                frm.ShowDialog();
+            }
+            else // Quản lý
+            {
+                ManagerMain frm = new ManagerMain();
+                frm.ShowDialog();
+            }
+
+            this.Hide();
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
